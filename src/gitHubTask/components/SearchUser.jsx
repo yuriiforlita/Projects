ad5651d578ac3fe1eefdaf779569d15df9b82718
Yura F <yurii.forlita@global.logic.com>
2018-11-21 12:11:58 +0200

    renaimingStyles

--- src/gitHubTask/components/SearchUser.jsx
+++ src/gitHubTask/components/SearchUser.jsx
@@ -3,8 +3,13 @@ import "./style.css";
 
 const SearchUser = ({ handleChange, handleSubmit }) => (
   <form className="search_wrapper" onSubmit={handleSubmit}>
-    <input onChange={handleChange} className="search_field" type="text" />
-    <button className="search_butt" type="submit">
+    <input
+      onChange={handleChange}
+      className="search_field"
+      placeholder="insert nick..."
+      type="text"
+    />
+    <button className="search_button" type="submit">
       Search..
     </button>
   </form>
